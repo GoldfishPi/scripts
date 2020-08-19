64100e1ff1456422335c91b24c54ce84d19d7c01
Erik Badger <erikbadger777@gmail.com>
2020-08-19 10:18:40 -0500

    changed monitor names

--- screen/monitor-manager.sh
+++ screen/monitor-manager.sh
@@ -9,10 +9,10 @@ selection=`echo -e "$single_monitor\n$dual_monitors"| dmenu -p "Monitors: "`
 
 if [ "$selection" == "$single_monitor" ] 
 then
-    exec xrandr --output DP-1 --off
+    exec xrandr --output DP1 --off
 fi
 
 if [ "$selection" == "$dual_monitors" ] 
 then
-    exec xrandr --output DP-1 --auto --above eDP-1
+    exec xrandr --output DP1 --auto --above eDP1
 fi
